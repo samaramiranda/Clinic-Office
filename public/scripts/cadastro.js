c1587e149c57862f228d1af488ed4b1511862d36
samaramiranda <samaramiranda.13@hotmail.com>
2020-08-22 23:46:29 -0300

    Adicionando API de buscar estados e cidades

--- public/scripts/cadastro.js
+++ public/scripts/cadastro.js
@@ -7,6 +7,7 @@ const phone = document.querySelector("#phone")
 const emailConfirm = document.querySelector("#emailConfirm")
 const button = document.querySelector("button")
 const state = document.querySelector("#state")
+const city = document.querySelector("#city")
 const inputs = [...document.querySelectorAll("input")]
 
 footer.innerHTML = `${footerContent()}`
@@ -20,12 +21,12 @@ const validateEmail = (event) => {
     clientEmail.parentNode.classList.remove("sucess")
     clientEmail.parentNode.classList.add("error")
     emailConfirm.disabled = true
-    button.disabled=true
+    button.disabled = true
   } else {
     clientEmail.parentNode.classList.remove("error")
     clientEmail.parentNode.classList.add("sucess")
     emailConfirm.disabled = false
-    button.disabled=false
+    button.disabled = false
   }
 }
 
@@ -35,12 +36,12 @@ const confirmEmail = (event) => {
     clientEmailConfirm.parentNode.classList.remove("error")
     clientEmailConfirm.parentNode.classList.add("sucess")
     email.parentNode.classList.add("sucess")
-    button.disabled=false
+    button.disabled = false
   } else {
     clientEmailConfirm.parentNode.classList.remove("sucess")
     // email.parentNode.classList.remove("sucess")
     clientEmailConfirm.parentNode.classList.add("error")
-    button.disabled=true
+    button.disabled = true
   }
 }
 
@@ -50,7 +51,7 @@ const validateFields = (event) => {
     return selectedField.parentNode.classList.add("error")
   } else if (selectedField.id === "email") {
     return validateEmail(event)
-  } else if (selectedField.id === "emailConfirm"){
+  } else if (selectedField.id === "emailConfirm") {
     return confirmEmail(event)
   }
   return selectedField.parentNode.classList.remove("error")
@@ -63,8 +64,44 @@ const phoneMask = () => {
   }
 }
 
+function populateUFs() {
+  const ufSelect = document.querySelector("#state")
+  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then(res => { 
+      return res.json()
+    })
+    .then(states => {
+      for (const elemState of states) {
+        ufSelect.innerHTML += `<option value="${elemState.id}">${elemState.nome}</option>`
+      }
+    })
+}
+
+populateUFs()
+
+function getCities(event) {
+  const citySelect = document.querySelector("#city")
+  const ufValue = event.target.value
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+  citySelect.innerHTML = '<option value="">Cidade</option>'
+
+  fetch(url)
+    .then(res => { 
+      return res.json()
+    })
+    .then(cities => {
+      for (const elemCity of cities) {
+        citySelect.innerHTML += `<option value="${elemCity.id}">${elemCity.nome}</option>`
+      }
+      citySelect.disabled = false
+    })
+}
+
+
 inputs.forEach(elem => {
   elem.addEventListener("focusout", validateFields)
 })
 phone.addEventListener("keyup", phoneMask)
+state.addEventListener("change", getCities)
 
