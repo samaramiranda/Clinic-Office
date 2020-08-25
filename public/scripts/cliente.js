435a241d6987b0002e113028f889df3f03d74513
samaramiranda <samaramiranda.13@hotmail.com>
2020-08-24 23:07:16 -0300

    Adicionando animação de descer a página

--- public/scripts/cliente.js
+++ public/scripts/cliente.js
@@ -1,13 +1,28 @@
 import footerContent from "./module-footer.js"
 
 const footer = document.querySelector("footer")
+const body = document.querySelector("body")
 const btnReturn = document.querySelector("#return")
 
 footer.innerHTML = `${footerContent()}`
 
 const returnIndex = () => {
   localStorage.removeItem('name')
-  location.href = "../../index.html"
+  body.classList.add("body-hide")
 }
+
+body.addEventListener("animationstart", event => {
+  if (event.animationName = "down") {
+    document.querySelector("body").style.overflow = "hidden"
+  }
+})
+
+body.addEventListener("animationend", event => {
+  if (event.animationName == "down") {
+    location.href = "../../index.html"
+    body.style.display = "none"
+    document.querySelector("body").style.overflow = "none"
+  }
+})
 
 btnReturn.addEventListener("click", returnIndex)
